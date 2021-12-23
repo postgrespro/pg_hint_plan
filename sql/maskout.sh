84c980556c8777387ed9f5f53c5f1d819677c863
Maxim Orlov <m.orlov@postgrespro.ru>
2021-12-23 19:13:56 +0300

    Add fix for cost output foramt
    For unknow reasons cost format was changed, so add
    sed format for maskout.
    
    Also add output for added autoprepare_for_protocol param.

--- sql/maskout.sh
+++ sql/maskout.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 cat $1 | \
 sed 's/cost=10\{7\}[\.0-9]\+ /cost={inf}..{inf} /;s/cost=[\.0-9]\+ /cost=xxx..xxx /;s/width=[0-9]\+\([^0-9]\)/width=xxx\1/;s/^ *QUERY PLAN *$/  QUERY PLAN/;s/^--*$/----------------/' |\
+sed 's/cost=10\{10\}[\.0-9]\+NaN/cost={inf}..{inf}/;' |\
+sed 's/cost=[\.0-9]\+NaN /cost=xxx..xxx /;' |\
 egrep -v "^ *((Planning time|JIT|Functions|Options):|\([0-9]* rows\))" |\
 sed -e 's/^ *QUERY PLAN *$/  QUERY PLAN/' -e 's/^--*$/----------------/'
